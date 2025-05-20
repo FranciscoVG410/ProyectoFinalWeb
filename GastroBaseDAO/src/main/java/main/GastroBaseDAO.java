efc269fbd9e329504ea5c0b553a3d524c9a6c379
Lunatikera <carlitosjuven@gmail.com>
2025-05-19 23:14:23 -0700

    push

--- GastroBaseDAO/src/main/java/main/GastroBaseDAO.java
+++ GastroBaseDAO/src/main/java/main/GastroBaseDAO.java
@@ -4,6 +4,13 @@
 
 package main;
 
+import DAOs.UsuarioDAO;
+import conexion.Conexion;
+import conexion.IConexionBD;
+import dominio.Chef;
+import dominio.Usuario;
+import interfaces.IUsuarioDAO;
+import java.time.LocalDate;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -15,15 +22,19 @@ import javax.persistence.Persistence;
 public class GastroBaseDAO {
 
     public static void main(String[] args) {
-        try {
-            EntityManagerFactory emf = Persistence.createEntityManagerFactory("GastroBase");
-            EntityManager em = emf.createEntityManager();
-            System.out.println("Database generated successfully!");
-            em.close();
-            emf.close();
-        } catch (Exception e) {
-            System.out.println("Error generating database: " + e.getMessage());
-            e.printStackTrace();
-        }
+        IConexionBD conexionBD= new Conexion();
+        IUsuarioDAO usuario= new UsuarioDAO(conexionBD);
+        
+          Chef chef= new Chef();
+          chef.setNombre("carlitos");
+          chef.setCorreo("carlitos@gmail.com");
+          chef.setRol(Usuario.Rol.CHEF);
+          chef.setApellido("rios");
+          chef.setContrasenia("123");
+          chef.setPais("Mexico");
+          chef.setTelefono("6683200668");
+          chef.setFechaNacimiento(LocalDate.of(2002, 12, 7));
+          
+          usuario.crearUsuario(chef);
     }
 }
